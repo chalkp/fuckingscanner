fdb74c0f8f23e82b8544de68e4e16f7f12847319
chalk <buzology@protonmail.com>
2022-11-21 07:20:31 +0700

    Update index.js

--- index.js
+++ index.js
@@ -15,20 +15,24 @@ servers.forEach(current => {
   let p = current.ports.map(a => a.port);
   p.forEach(po => {
     const sv = new server(current.ip, po.port);
-    sv.ping(5000, 720, async function (err, res) {
+    sv.ping(50000, 720, async function (err, res) {
       try {
         if(typeof err === 'undefined' || err === null) {
           console.log(`${current.ip} => description: ${res.description.text}`);
           fs.appendFile('output.txt', `${current.ip} => description: ${res.description.text}\n` ,(err) => {
             if (err) throw err;
           });
-          if(res.description.text.includes("LiveOverflow")) {
+          const r1 = res.description.text?.includes("LiveOverflow");
+          if(r1) {
             fs.appendFile('found.txt', `${current.ip} => description: ${res.description.text}\n` ,(err) => {
               if (err) throw err;
             });
           }
         } else {
           console.log(current.ip + " offline");
+          fs.appendFile('output.txt', `${current.ip} => offline\n` ,(err) => {
+            if (err) throw err;
+          });
         }
       } catch(e) {
         console.log(e);
